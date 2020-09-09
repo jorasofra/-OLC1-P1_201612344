4faed71a05c2c10ec73c74975ecddfea73c2f787
José Soli <jorasofra@gmail.com>
2020-09-09 17:43:28 -0600

    Ventana Creada

--- __init__.py
+++ __init__.py
@@ -2,5 +2,12 @@ import Token
 import TipoToken
 import AnalizadorHTML
 from Ventana import Ventana
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
-v = Ventana()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    GUI = Ventana()
+    GUI.show()
+    sys.exit(app.exec_())
